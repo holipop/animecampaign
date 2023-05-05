ba5e2f50aec9876fb0a685b94b23a10d793e799d
Sunny <adamcroslow@gmail.com>
2023-05-05 00:14:40 -0500

    kit piece is now a type of item

--- module/sheets/ACItemSheet.js
+++ module/sheets/ACItemSheet.js
@@ -9,17 +9,15 @@ export default class ACItemSheet extends ItemSheet {
     }
 
     get template() {
-        let kit = ['Weapon', 'Talent', 'Passive', 'Ability'];
-        if (kit.includes(this.item.type)) {
-            return `systems/animecampaign/templates/sheets/kit-piece-sheet.hbs`;
-        }
         return `systems/animecampaign/templates/sheets/${this.item.type}-sheet.hbs`;
     }
 
     async getData() {
         const data = super.getData()
+
         data.config = CONFIG.animecampaign; //This is the localization
         data.system = data.item.system; //THIS IS THE SHIT WE DEFINED!!!
+        
         return data;
     }
 
