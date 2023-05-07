dd4f40b9c2e8917d15d5fc5fbded0d9158e1bb1f
Sunny <adamcroslow@gmail.com>
2023-05-07 16:28:47 -0500

    added deleteAllStats() method

--- module/config.js
+++ module/config.js
@@ -19,19 +19,29 @@ export const ACEntityMixin = {
 
         ui.notifications.info(`Anime Campaign | Deleted the "${key}" stat on ${this.parent.name}.`);
         this.parent.update({ [`system.stats.-=${key}`]: null })
+    },
+
+    deleteAllStats() {
+        const stats = Object.keys(this.parent.system.stats);
+
+        if (stats.length == 0) {
+            ui.notifications.error(`Anime Campaign | ${this.parent.name} doesn't have any stats.`);
+            return;
+        }
+
+        ui.notifications.info(`Anime Campaign | Deleted all stats on ${this.parent.name}.`);
+        stats.forEach(element => this.parent.update({ [`system.stats.-=${element}`]: null }));
     }
 }
 
 export const animecampaign = {};
 
 animecampaign.test = "animecampaign.test"
-
 animecampaign.build = {
     svelte: "animecampaign.build.svelte",
     average: "animecampaign.build.average",
     heavyset: "animecampaign.build.heavyset",
 }
-
 animecampaign.type = {
     inscribed: {
         epithet: "animecampaign.type.inscribed.epithet"
@@ -41,19 +51,16 @@ animecampaign.type = {
         powerhouse: "animecampaign.type.mundie.powerhouse"
     }
 }
-
 animecampaign.kitTypes = {
     weapon: "animecampaign.kitTypes.weapon",
     talent: "animecampaign.kitTypes.talent",
     passive: "animecampaign.kitTypes.passive",
     ability: "animecampaign.kitTypes.ability"
 }
-
 animecampaign.powerset = {
     inscribed: "animecampaign.powerset.inscribed",
     mundie: "animecampaign.powerset.mundie"
 }
-
 animecampaign.statLabels = {
     stamina: "animecampaign.statLabels.stamina",
     proficiency: "animecampaign.statLabels.proficiency",
@@ -64,7 +71,6 @@ animecampaign.statLabels = {
     bonus: "animecampaign.statLabels.bonus",
     action: "animecampaign.statLabels.action"
 }
-
 animecampaign.navigationLabels = {
     kit: "animecampaign.navigationLabels.kit",
     upgrades: "animecampaign.navigationLabels.upgrades",
