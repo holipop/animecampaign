bc1a8692d556d28c32059db847cdb750aae51cf3
zanza-Klaus <43688729+zanza-Klaus@users.noreply.github.com>
2023-05-09 17:34:23 -0700

    Adding kit pieces to character sheets
    You can now drag and drop kit piece items on to character sheets. This adds them to the "kit" section of their sheet.
    
    Currently it only displays the name of the feature with no additional functionality.
    
    - Created kit-piece-card.hbs file to manage each individual kit piece displayed
    - Changed kit.hbs to simply display all items with kit-piece-card.hbs
    - Created kit-piece-card.less, though it currently has no styling in it
    - Added kit-piece-card.hbs to animecampaign.js and kit-piece-card.less to animecampaign.less

--- animecampaign.js
+++ animecampaign.js
@@ -11,6 +11,7 @@ async function preloadHandlebarsTemplates() {
         "systems/animecampaign/templates/sheets/partials/character-summary.hbs",
         "systems/animecampaign/templates/sheets/partials/character-stats.hbs",
         "systems/animecampaign/templates/sheets/partials/kit.hbs",
+        "systems/animecampaign/templates/sheets/partials/kit-piece-card.hbs",
         "systems/animecampaign/templates/sheets/partials/upgrades.hbs",
         "systems/animecampaign/templates/sheets/partials/biography.hbs"
     ];
