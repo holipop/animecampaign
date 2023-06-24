a43d890b860ad425f8a6087ec1b1c41485d5e904
Sunny <adamcroslow@gmail.com>
2023-06-24 00:08:01 -0500

    made the Sections of Kit Pieces look pretty

--- module/data-models/Section.js
+++ module/data-models/Section.js
@@ -3,7 +3,7 @@ export class Section extends foundry.abstract.DataModel {
         const fields = foundry.data.fields;
 
         return {
-            name: new fields.StringField(),
+            label: new fields.StringField(),
             value: new fields.StringField(),
             stat: new fields.StringField(),
             text: new fields.HTMLField(),
