ff72e516ac125c74f131bd1a8369afddbd769e11
Sunny <adamcroslow@gmail.com>
2023-07-09 21:45:14 -0500

    JSDoc replacing comments

--- animecampaign.js
+++ animecampaign.js
@@ -1,66 +1,73 @@
-import { animecampaign } from "./module/config.js";
+// Helper and Config 
 import AC from "./module/AC.js";
-
-import ACActor from "./module/documents/ACActor.js";
-import CharacterSheet from "./module/sheets/CharacterSheet.js";
-import { CharacterData } from "./module/data-models/CharacterData.js";
-
-import ACItem from "./module/documents/ACItem.js";
-import KitPieceSheet from "./module/sheets/KitPieceSheet.js";
-import { KitPieceData } from "./module/data-models/KitPieceData.js";
-
+import { animecampaign } from "./module/config.js";
 import { RolledItem } from "./module/RolledItem.js";
 
-//  Preloads the filepaths for the Handlebars partials.
-//*     () : Promise<Function[]>
-async function preloadHandlebarsTemplates() {
-    const templatePaths = [
-        "systems/animecampaign/templates/sheets/partials/character-summary.hbs",
-        "systems/animecampaign/templates/sheets/partials/stats.hbs",
-        "systems/animecampaign/templates/sheets/partials/sections.hbs",
-        "systems/animecampaign/templates/sheets/partials/kit.hbs",
-        "systems/animecampaign/templates/sheets/partials/upgrades.hbs",
-        "systems/animecampaign/templates/sheets/partials/biography.hbs",
-    ];
+// Documents
+import ACActor from "./module/documents/ACActor.js";
+import ACItem from "./module/documents/ACItem.js";
 
-    return loadTemplates(templatePaths);
+// Data Models
+import { CharacterData } from "./module/data-models/CharacterData.js";
+import { KitPieceData } from "./module/data-models/KitPieceData.js";
+
+// Applications
+import CharacterSheet from "./module/sheets/CharacterSheet.js";
+import KitPieceSheet from "./module/sheets/KitPieceSheet.js";
+
+/** Preloads any .hbs files to be used as partials.
+ * @async
+ * @returns {Promise<Function[]>}
+ */
+async function preloadHandlebarsTemplates() {
+  // Whenever you create a new .hbs in the "partials" folder, put the filepath here.
+  const templatePaths = [
+    "systems/animecampaign/templates/sheets/partials/character-summary.hbs",
+    "systems/animecampaign/templates/sheets/partials/stats.hbs",
+    "systems/animecampaign/templates/sheets/partials/sections.hbs",
+    "systems/animecampaign/templates/sheets/partials/kit.hbs",
+    "systems/animecampaign/templates/sheets/partials/upgrades.hbs",
+    "systems/animecampaign/templates/sheets/partials/biography.hbs",
+  ];
+
+  return loadTemplates(templatePaths);
 }
 
-//  All of our code that runs on initialization.
+// All of our code that runs on initialization.
 Hooks.once("init", () => {
-    AC.log("Initializing Anime Campaign System");
-    
-    //  Adding our localization object to Foundry's CONFIG object.
-    CONFIG.animecampaign = animecampaign;
+  AC.log("Initializing Anime Campaign System");
 
-    //  Assigning diagonal rule.
-    SquareGrid.prototype.measureDistances = AC.measureDistances;
+  // Adding the localization object to Foundry's CONFIG object.
+  CONFIG.animecampaign = animecampaign;
 
-    //  Assigning Fonts
-    CONFIG.fontDefinitions = { ...CONFIG.fontDefinitions, ...AC.fonts };
-    CONFIG.defaultFontFamily = 'Arial';
+  // Assigning diagonal rule.
+  SquareGrid.prototype.measureDistances = AC.measureDistances;
 
-    //  Redefining the default document classes.
-    CONFIG.Actor.documentClass = ACActor;
-    CONFIG.Item.documentClass = ACItem;
+  // Assigning Fonts
+  CONFIG.fontDefinitions = { ...CONFIG.fontDefinitions, ...AC.fonts };
+  CONFIG.defaultFontFamily = 'Arial';
 
-    //  Assigning Character and Kit Piece schema.
-    CONFIG.Actor.dataModels["Character"] = CharacterData;
-    CONFIG.Item.dataModels["Kit Piece"] = KitPieceData;
+  // Redefining the default document classes.
+  CONFIG.Actor.documentClass = ACActor;
+  CONFIG.Item.documentClass = ACItem;
 
-    //  Unregistering the default document sheets & registering our own.
-    Actors.unregisterSheet("core", ActorSheet);
-    Actors.registerSheet("animecampaign", CharacterSheet, { makeDefault: true });
-    Items.unregisterSheet("core", ItemSheet);
-    Items.registerSheet("animecampaign", KitPieceSheet, { makeDefault: true });
+  // Assigning Character and Kit Piece schema.
+  CONFIG.Actor.dataModels["Character"] = CharacterData;
+  CONFIG.Item.dataModels["Kit Piece"] = KitPieceData;
 
-    preloadHandlebarsTemplates();
+  // Unregistering the default document sheets & registering our own.
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("animecampaign", CharacterSheet, { makeDefault: true });
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet("animecampaign", KitPieceSheet, { makeDefault: true });
 
-    //  Adding our custom Handlebars helpers.
-    Handlebars.registerHelper(AC.hbsHelpers);
+  preloadHandlebarsTemplates();
+
+  // Adding our custom Handlebars helpers.
+  Handlebars.registerHelper(AC.hbsHelpers);
 })
 
-//  All of the code that runs for chat messages.
+// All of the code that runs for chat messages.
 Hooks.on('renderChatMessage', (_app, _html, _data) => {
-    RolledItem.addChatListeners(_html);
+  RolledItem.addChatListeners(_html);
 })
