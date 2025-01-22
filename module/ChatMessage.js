dd5b70db6dbf1dec1f91ad98235f728cc07b1190
Sunny <adamcroslow@gmail.com>
2025-01-22 04:18:55 -0600

    basic pseudo sections

--- module/ChatMessage.js
+++ module/ChatMessage.js
@@ -4,36 +4,59 @@
 
 
 /** Event listeners for chat messages.
- * @param {*} message 
- * @param {*} html 
+ * @param {ChatMessage} message 
+ * @param {JQuery} html 
  * @param {*} data 
  */
 export function activateListeners (message, html, data) {
 
-    const msg = html.find('[data-id]');
+    const messageElement = html[0].querySelector(".JS-ChatMessage")
+    if (!messageElement) return
 
-    // Collapses an element given a button and target.
-    msg.find('[data-collapse]')
-        .each((index, element) => { 
-            // Set the initial state of visibility
-            const anchor = $(element).closest('a');
-            const key = anchor.data('collapse');
-            const visibile = anchor.data('visible');
-            const target = msg.find(`[data-collapse-target="${key}"]`);
+    // Expand roll tooltip on click
+    const roll = messageElement.querySelector(".JS-ExpandTooltip")
+    if (roll) {
+        const tooltip = messageElement.querySelector("div.dice-tooltip")
 
-            if (visibile) return;
-
-            target.toggle();
-            anchor.find('i').toggleClass('fa-chevron-down');
-            anchor.find('i').toggleClass('fa-chevron-right');
+        roll.addEventListener("click", event => {
+            const display = tooltip.style.display
+            tooltip.style.display = (display) ? "" : "block"
         })
-        .on('click', event => {
-            const anchor = $(event.target).closest('a');
-            const key = anchor.data('collapse');
-            const target = msg.find(`[data-collapse-target="${key}"]`);
+    }
 
-            target.toggle();
-            anchor.find('i').toggleClass('fa-chevron-down');
-            anchor.find('i').toggleClass('fa-chevron-right');
+    // Inject sections
+    const headers = ["h1", "h2", "h3", "h4", "h5", "h6"]
+    const query = headers.map(v => `.JS-Content ${v}`).join()
+
+    /** @type {NodeListOf<HTMLElement>} */
+    const sections = messageElement.querySelectorAll(query)
+    if (sections) {
+        sections.forEach((header, index, parent) => {
+            // Wrap siblings in a Section div
+            const siblings = []
+            let sibling = header.nextElementSibling
+
+            while (sibling) {
+                if (headers.map(v => v.toUpperCase()).includes(sibling.nodeName)) break
+                siblings.push(sibling)
+                if (sibling.nodeName == "HR") break
+
+                sibling = sibling.nextElementSibling
+            }
+
+            const section = document.createElement("div")
+            section.classList.add("JS-SectionContent")
+            section.hidden = header.textContent.startsWith("<")
+            siblings.forEach(v => section.append(v))
+            
+            header.insertAdjacentElement("afterend", section)
+
+            //! TODO: CLEAN THIS UP, MAKE IT NEATER
+
+            header.addEventListener("click", () => {
+                section.hidden = !section.hidden
+            })
         })
+    }
+    
 }
