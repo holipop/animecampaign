3015f5f41917bdc9812851d7fbc1704079fb4b90
Sunny <adamcroslow@gmail.com>
2025-10-17 02:19:57 -0500

    cleanup

--- animecampaign.js
+++ animecampaign.js
@@ -24,6 +24,9 @@ globalThis.AC = {
     Macro: { ...Macro }
 }
 
+/**
+ * System Initialization
+ */
 Hooks.once('init', () => {
     console.log(`%cAnime Campaign | ${config.AC.ascii}`, 'color: #db7093;');
     console.log("%cAnime Campaign | Initializing Anime Campaign System!", "color: #db7093;");
@@ -72,6 +75,9 @@ Hooks.once('init', () => {
     loadTemplates(partials)
 })
 
+/**
+ * Migration Handling
+ */
 Hooks.on('ready', () => {
     const NEEDS_MIGRATION_VERSION = "v1.0"
     const currentVersion = game.settings.get('animecampaign', 'systemMigrationVersion')
@@ -99,6 +105,9 @@ Hooks.on('ready', () => {
     game.settings.set("animecampaign", "systemMigrationVersion", game.system.version)
 })
 
+/**
+ * Attach Buttons to Chat Messages
+ */
 Hooks.on('renderChatMessage', (message, html, data) => {
     const messageElement = html[0].querySelector(".JS-ChatMessage")
     if (!messageElement) return
@@ -120,11 +129,8 @@ Hooks.on('renderChatMessage', (message, html, data) => {
 
 Hooks.on('hotbarDrop', Macro.createMacro)
 
-
-
 /**
- * Attempt #3 in my own style, specifically for overriding.
- * Holy shit.
+ * ProseMirror Overriding
  */
 Hooks.on("init", () => {
     // override headings to allow for toggling section visibility
@@ -174,13 +180,9 @@ Hooks.on("init", () => {
 })
 
 Hooks.on('getProseMirrorMenuItems', (menu, items) => {
-    console.log(menu)
-
     const isEditingFeature = menu.view.dom.closest(".JS-IsFeature")
     const isEditingCharacter = menu.view.dom.closest(".JS-IsCharacter")
     if (!isEditingFeature && !isEditingCharacter) return
-
-    console.log("PASSED")
 
     menu.items = items.unshift({
         action: "toggle-visibility",
@@ -198,132 +200,4 @@ Hooks.on('getProseMirrorMenuItems', (menu, items) => {
             return true
         }
     })
-})
-
-/** 
- * Attempt #2 with ProseMirror Editing 
- * Credit to joaquinp98 in the Foundry server for this
-*/
-/* Hooks.on("init", () => {
-    const { defaultSchema, Schema } = foundry.prosemirror;
-    const { deepClone } = foundry.utils;
-    
-    const colorMark = {
-        inclusive: true,
-        excludes: "span", // this is essential 
-        // ProseMirror parses <span style="color:red"> as both a span mark and a color mark
-        parseDOM: [
-            { style: "color=red" }
-        ],
-        toDOM: () => ["span", { style: "color: red" }, 0],
-    };
-
-    const extendedSchema = new Schema({
-        nodes: defaultSchema.spec.nodes,
-        marks: deepClone(defaultSchema.spec.marks).addToStart("colorMark", colorMark),
-    });
-
-    Object.assign(foundry.prosemirror.defaultSchema, extendedSchema);
-});
-
-Hooks.on("getProseMirrorMenuItems",(menu, config) => {
-    const { TEXT } = foundry.prosemirror.ProseMirrorMenu._MENU_ITEM_SCOPES;
-    const { toggleMark } = foundry.prosemirror.commands;
-
-    config.push({
-        action: "text-color",
-        title: "Text Color",
-        mark: menu.schema.marks.colorMark,
-        icon: `<i class="fa-solid fa-palette"></i>`,
-        scope: TEXT,
-        cmd: toggleMark(menu.schema.marks.colorMark),
-    });
-  
-}); */
-
-
-/**
- * This was an attempt at making buttons inside the prosemirror editor for marking sections as hidden.
- * No matter what, it doesn't really like to budge.
- */
-Hooks.on('createProseMirrorEditor', (uuid, plugins, options) => {
-    //console.log(uuid, plugins, options)
-
-    /* // override headings to allow for toggling section visibility
-    const heading = {
-        attrs: {
-            level: {default: 1},
-            hidden: {default: false},
-        },
-        content: "inline*",
-        group: "block",
-        defining: true,
-        parseDOM: [
-            {tag: "h1", attrs: {level: 1}},
-            {tag: "h2", attrs: {level: 2}},
-            {tag: "h3", attrs: {level: 3}},
-            {tag: "h4", attrs: {level: 4}},
-            {tag: "h5", attrs: {level: 5}},
-            {tag: "h6", attrs: {level: 6}}
-        ],
-        toDOM: node => [`h${node.attrs.level}`, { 
-            "attr-hide": (node.attrs.hidden) ? true : false, 
-            style: `opacity: ${(node.attrs.hidden) ? .75 : 1};`
-        }, 0 ]
-    }
-
-    const schema = new ProseMirror.Schema({
-        nodes: options.state.schema.spec.nodes.update("heading", heading),
-        marks: options.state.schema.spec.marks
-    })
-
-    // override the editor state with new content
-    const content = ProseMirror.dom.serializeString(options.state.doc, { schema })
-    const state = ProseMirror.EditorState.create({ doc: ProseMirror.dom.parseString(content) })
-
-    console.log(content, state) */
-
-    /* const serializer = ProseMirror.DOMSerializer.fromSchema(schema)
-    const html = serializer.serializeFragment(options.state.doc.content)
-    console.log(html)
-
-    options.state = ProseMirror.EditorState.create({
-        doc: ProseMirror.DOMParser.fromSchema(schema).parse(html),
-        plugins: options.state.plugins
-    }) */
-
-    /* const state = ProseMirror.EditorState.create({ schema, plugins: options.state.plugins })
-
-    state.doc = options.state.doc
-    options.state = state*/
-})
-
-Hooks.on('getProseMirrorMenuItems', (menu, items) => {
-    //console.log(menu, items)
-
-    /* menu.items = items.unshift({
-        action: "toggle-visibility",
-        title: "AC.ToggleVisibility",
-        icon: '<i class="fa-solid fa-eye-slash"></i>',
-        scope: "text",
-        cmd: (state, dispatch, view) => {
-            console.log(state, dispatch, view)
-
-            // get header node
-            const { $from } = state.selection
-            const node = $from.parent
-
-            if (!node || node.type.name != "heading" ) return false
-            console.log(node)
-
-            if (dispatch) {
-                //console.log(state.tr.setNodeAttribute(node, "_preserve", { "style": "color:red;" }))
-                //dispatch(state.tr.setNodeAttribute(state.selection.from, "_preserve", { "style": "color:red;" }))
-            }
-
-            /* if (state.selection.empty) return false
-            if (dispatch) dispatch(state.tr.insertText("sexo!!").scrollIntoView())
-            return true
-        }
-    }) */
 })
